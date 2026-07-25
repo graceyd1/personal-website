4dacd90745fff478102b5a15865d52a4b2073dbf
graceyd1 <215888876+graceyd1@users.noreply.github.com>
2026-07-24 23:59:41 -0600

    New video notes page started

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ for (var i = 0; i < collapsables.length; i++) {
   collapsables[i].addEventListener("click", toggleCollapsible);
 } 
 
+var collapsables2 = document.getElementsByClassName("video-notes-button");
+for (var i = 0; i < collapsables2.length; i++) {
+  collapsables2[i].addEventListener("click", toggleCollapsible);
+}
+
 function toggleCollapsible() {
-    this.classList.toggle("active");
-    var body = this.nextElementSibling;
-    if (body.style.display === "block") 
-    {
-      body.style.display = "none";
-    }
-    else 
-    {
-      body.style.display = "block";
-    }
+  console.log("click");
+  this.classList.toggle("active");
+  var body = this.nextElementSibling;
+  if (body.style.display === "block") 
+  {
+    body.style.display = "none";
+  }
+  else 
+  {
+    body.style.display = "block";
+  }
 }
